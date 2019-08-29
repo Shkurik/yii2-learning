49eda2d966eab412a318acb55e1bace23929760d
Oleksandr Shkuropat <oleksandr.shkuropat@gmail.com>
2019-08-29 13:55:14 +0300

    Program entry point

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # yii2
+1) создать web/index.php
+  
+        require  __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+        $config = require __DIR__. '/../config/web.php';
+        (new yii\web\Application($config))->run();
+        
+2) создать .gitignore
+
+        .idea
+        vendor
+    
+3) запустить комманду
+        
+        composer global require "fxp/composer-asset-plugin:*"
+        composer require "yiisoft/yii2:*"
+        
+4) создать config/web.php
+>
+    return [
+        'id' => 'school',
+        'basePath' => realpath(__DIR__ . '/../')
+    ];
+
+5)
--- web/index.php
+++ web/index.php
@@ -1,4 +1,16 @@
 <?php
-    require  __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-    $config = require __DIR__. '/../config/web.php';
-    (new yii\web\Application($config))->run();
+if (!defined('YII_DEBUG')) {
+    define('YII_DEBUG', true);
+}
+
+// регистрация загрузчика классов Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// подключение файла класса Yii
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+// загрузка конфигурации приложения
+$config = require __DIR__ . '/../config/web.php';
+
+// создание и конфигурация приложения, а также вызов метода для обработки входящего запроса
+(new yii\web\Application($config))->run();
