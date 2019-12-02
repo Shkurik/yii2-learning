87fe068936c9a862387774e44b9edc4cb0c8d928
Oleksandr Shkuropat <oleksandr.s@giantleaplab.com>
2019-12-02 19:30:50 +0200

    add nav items

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,4 +7,12 @@ class SiteController extends Controller
     public function actionIndex(){
         return $this->render('index');
     }
+
+    public function actionJoin(){
+        return $this->render('index');
+    }
+
+    public function actionLogin(){
+        return $this->render('index');
+    }
 }
--- web/index.php
+++ web/index.php
@@ -4,8 +4,8 @@ if (!defined('YII_DEBUG')) {
     define('YII_DEBUG', true);
 }
 
-//// регистрация загрузчика классов Composer
-//require __DIR__ . '/../vendor/autoload.php';
+// регистрация загрузчика классов Composer
+require __DIR__ . '/../vendor/autoload.php';
 
 // подключение файла класса Yii
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
