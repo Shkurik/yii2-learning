304f376806748c939b34a2f812c2f542fac7aa05
Oleksandr Shkuropat <oleksandr.s@giantleaplab.com>
2019-12-02 18:28:42 +0200

    add views-site-index and layouts-main

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -5,6 +5,6 @@ use yii\web\Controller;
 class SiteController extends Controller
 {
     public function actionIndex(){
-        return 'VideoSchool';
+        return $this->render('index');
     }
 }
--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
 <?php
 
-//if (!defined('YII_DEBUG')) {
-//    define('YII_DEBUG', true);
-//}
-//
+if (!defined('YII_DEBUG')) {
+    define('YII_DEBUG', true);
+}
+
 //// регистрация загрузчика классов Composer
 //require __DIR__ . '/../vendor/autoload.php';
-//
+
 // подключение файла класса Yii
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
