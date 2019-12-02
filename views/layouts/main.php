3ee65219d2b7d2c275f2549834a64e3b1434658a
Oleksandr Shkuropat <oleksandr.s@giantleaplab.com>
2019-12-02 19:19:49 +0200

    add bootstrap and navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,12 @@
+<?php
+    use yii\bootstrap\NavBar;
+?>
+<?php $this->beginPage(); ?>
+
 <html lang="en">
 <head>
+    <?php $this->head(); ?>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -7,6 +14,23 @@
     <title>VideoSchool</title>
 </head>
 <body>
-    <?= $content ?>
+    <?php $this->beginBody(); ?>
+
+    <?php
+        NavBar::begin([
+            'brandLabel' => 'VideoSchool',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-default navbar-fixed-top'
+            ]
+        ]);
+        NavBar::end();
+    ?>
+    <div class="container" style="margin-top: 80px">
+        <?= $content ?>
+    </div>
+    <?php $this->endBody(); ?>
+
 </body>
 </html>
+<?php $this->endPage(); ?>
