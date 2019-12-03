8d55ce6b47af50eff0cf2f91403169e039014889
Oleksandr Shkuropat <oleksandr.s@giantleaplab.com>
2019-12-03 12:38:28 +0200

    describe join and login pages

--- views/user/join.php
+++ views/user/join.php
@@ -1,1 +1,12 @@
-<h1>Join us</h1>
+<div class="panel panel-danger">
+    <div class="panel-heading">
+        <h1>Join us</h1>
+    </div>
+    <div class="panel-body">
+        name </br>
+        email </br>
+        password </br>
+        password2 </br>
+        [create]
+    </div>
+</div>
--- views/user/login.php
+++ views/user/login.php
@@ -1,1 +1,10 @@
-<h1>Lon in</h1>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h1>Lon in</h1>
+    </div>
+    <div class="panel-body">
+        email </br>
+        password </br>
+        [Enter]
+    </div>
+</div>
