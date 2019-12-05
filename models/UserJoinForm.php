b48a4f25c268b637c0fdd1d8829b68988c8969e5
Oleksandr Shkuropat <oleksandr.s@giantleaplab.com>
2019-12-05 16:21:19 +0200

    add userJoinForm rules

--- models/UserJoinForm.php
+++ models/UserJoinForm.php
@@ -10,4 +10,18 @@ class UserJoinForm extends Model
     public $email;
     public $password;
     public $password2;
+
+    public function rules()
+    {
+        return [
+            ['name', 'required'],
+            ['email', 'required'],
+            ['password', 'required'],
+            ['password2', 'required'],
+            ['name', 'string', 'min' => 3, 'max' => 30],
+            ['email', 'email'/*, 'message' => 'email is not valid'*/],
+            ['password', 'string', 'min' => 4],
+            ['password2', 'compare', 'compareAttribute' => 'password'],
+        ];
+    }
 }
